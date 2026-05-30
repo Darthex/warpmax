93f17f5d3f005cacbfdf9df9f58b2ba9e5163d9e
Harshul Sharma <76007809+Darthex@users.noreply.github.com>
2026-05-31 02:00:22 +0530

    Camera setup (#3)

---
+++ src/core/camera.rs
@@ -0,0 +1,14 @@
+﻿use crate::utilities::constants::CLEAR_COLOR;
+use bevy::prelude::*;
+
+pub struct CameraPlugin;
+impl Plugin for CameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(ClearColor(CLEAR_COLOR))
+            .add_systems(Startup, setup_camera);
+    }
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((Camera2d,));
+}
---
+++ src/core/window.rs
@@ -0,0 +1,15 @@
+﻿use crate::utilities::constants::{CANVAS_HEIGHT, CANVAS_WIDTH, GAME_NAME};
+use bevy::prelude::{Window, WindowPlugin, default};
+use bevy::window::{WindowResolution, WindowTheme};
+
+pub fn get_window_plugin() -> WindowPlugin {
+    WindowPlugin {
+        primary_window: Some(Window {
+            title: String::from(GAME_NAME),
+            resolution: WindowResolution::new(CANVAS_WIDTH, CANVAS_HEIGHT),
+            window_theme: Some(WindowTheme::Dark),
+            ..default()
+        }),
+        ..default()
+    }
+}
