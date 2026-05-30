b691ba124e89d01e6fa6411e87975f8de830a053
Darthex <mrharshulsharma@@gmail.com>
2026-05-31 01:27:57 +0530

    Setup

---
+++ src/plugins.rs
@@ -0,0 +1,18 @@
+﻿use crate::utilities::constants::{CANVAS_HEIGHT, CANVAS_WIDTH, GAME_NAME};
+use bevy::prelude::*;
+use bevy::window::WindowResolution;
+
+// Plugin registry
+pub struct GamePlugins;
+impl Plugin for GamePlugins {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: String::from(GAME_NAME),
+                resolution: WindowResolution::new(CANVAS_WIDTH, CANVAS_HEIGHT),
+                ..default()
+            }),
+            ..default()
+        }));
+    }
+}
---
+++ src/utilities/constants.rs
@@ -0,0 +1,6 @@
+﻿use bevy::color::Color;
+
+pub const GAME_NAME: &str = "Warpmax";
+pub const CLEAR_COLOR: Color = Color::srgb_u8(168, 191, 138);
+pub const CANVAS_WIDTH: u32 = 1280;
+pub const CANVAS_HEIGHT: u32 = 720;
