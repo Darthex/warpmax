26bdec0058ca6240a5a898e5f676a6c68f8d0319
Harshul Sharma <76007809+Darthex@users.noreply.github.com>
2026-05-31 01:28:55 +0530

    Setup (#2)
    Co-authored-by: Darthex <mrharshulsharma@@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+mod plugins;
+mod utilities;
+mod scenes;
+
+use crate::plugins::GamePlugins;
+use crate::utilities::constants::CLEAR_COLOR;
 use bevy::prelude::*;
 
 fn main() -> AppExit {
-    App::new().add_plugins(DefaultPlugins).run()
+    App::new()
+        .insert_resource(ClearColor(CLEAR_COLOR))
+        .add_plugins(GamePlugins)
+        .run()
 }
