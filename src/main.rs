09f638ee80d99db782da836d2f1121f5fb0f2965
Darthex <mrharshulsharma@@gmail.com>
2026-05-31 01:59:20 +0530

    Camera setup

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
+mod core;
 mod plugins;
-mod utilities;
 mod scenes;
+mod utilities;
 
 use crate::plugins::GamePlugins;
-use crate::utilities::constants::CLEAR_COLOR;
 use bevy::prelude::*;
 
 fn main() -> AppExit {
     App::new()
-        .insert_resource(ClearColor(CLEAR_COLOR))
         .add_plugins(GamePlugins)
         .run()
 }
--- src/plugins.rs
+++ src/plugins.rs
@@ -1,18 +1,12 @@
-﻿use crate::utilities::constants::{CANVAS_HEIGHT, CANVAS_WIDTH, GAME_NAME};
+﻿use crate::core::camera::CameraPlugin;
+use crate::core::window::get_window_plugin;
 use bevy::prelude::*;
-use bevy::window::WindowResolution;
 
 // Plugin registry
 pub struct GamePlugins;
 impl Plugin for GamePlugins {
     fn build(&self, app: &mut App) {
-        app.add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: String::from(GAME_NAME),
-                resolution: WindowResolution::new(CANVAS_WIDTH, CANVAS_HEIGHT),
-                ..default()
-            }),
-            ..default()
-        }));
+        app.add_plugins(DefaultPlugins.set(get_window_plugin()))
+            .add_plugins(CameraPlugin);
     }
 }
