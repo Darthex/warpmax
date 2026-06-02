2f27da6fa19973e6dcd28e6629239fb6b2aecdf9
drwnkxx <kalpitkhajuria@gmail.com>
2026-06-02 22:38:02 +0530

    hello

--- src/components/player.rs
+++ src/components/player.rs
@@ -6,48 +6,29 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(State::Playing), spawn_player)
-            .add_systems(Update, player_movement.run_if(in_state(State::Playing)));
+        app.add_systems(OnEnter(State::Loading), spawn_player)
+            .add_systems(Update, player_movement.run_if(in_state(State::Loading)));
     }
 }
 
 
 //constant values
-const SPEED: f32 = 100.0;
-
-
+const SPEED: f32 = 600.0;
 //structs & enums
 #[derive(Component)]
 struct Player;
-
-#[derive(Component, Clone, Copy, PartialEq, Eq)]
-enum Facing {
-    Left,
-    Right,
-    Up,
-    Down,
-}
-
-#[derive(Component)]
-struct AnimationState {
-    facing: Facing,
-    is_moving: bool,
-    is_idle: bool,
-} 
 
 
 fn spawn_player(
     mut commands: Commands,
     asset_server: Res<AssetServer>,)
-{
-    let facing = Facing::Up;
-    let is_moving = false;
-    let is_idle = true;
-
-    //spawns an entity and attaches following components on top of it
+{   //spawns an entity and attaches following components on top of it
     commands.spawn((
-        Sprite::from_image(asset_server.load("sprites/player.png")),
-        AnimationState { facing, is_moving, is_idle },
+        Sprite {
+            custom_size: Some(Vec2::splat(50.)),
+            image: asset_server.load("sprites/player.png"),  
+            ..default()
+        },  
         Transform::from_translation(Vec3::ZERO),
         Player,
     ));
@@ -75,7 +56,6 @@ fn player_movement(
     }
     
     if direction != Vec2::ZERO {
-        
         let delta = direction.normalize() * SPEED * time.delta_secs();
         transform.translation.x += delta.x;
         transform.translation.y += delta.y;
